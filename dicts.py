a2c5b6538f926035a1c3a7a57b48d9af9eba76df
JudithT <administrator@Christophers-MacBook-Pro.local>
2019-04-21 20:34:27 -0700

    update

--- dicts.py
+++ dicts.py
@@ -4,125 +4,125 @@
 dictionaries and sets.
 """
 
-# def count_words(phrase):
-#     """Count unique words in a string.
+def count_words(phrase):
+    """Count unique words in a string.
 
-#     This function should take a single string and return a dictionary
-#     that has all of the distinct words as keys and the number of
-#     times that word appears in the string as values.
+    This function should take a single string and return a dictionary
+    that has all of the distinct words as keys and the number of
+    times that word appears in the string as values.
 
-#     For example::
+    For example::
 
-#         >>> print_dict(count_words("each word appears once"))
-#         {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
+        >>> print_dict(count_words("each word appears once"))
+        {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
 
-#     Words that appear more than once should be counted each time::
+    Words that appear more than once should be counted each time::
 
-#         >>> print_dict(count_words("rose is a rose is a rose"))
-#         {'a': 2, 'is': 2, 'rose': 3}
+        >>> print_dict(count_words("rose is a rose is a rose"))
+        {'a': 2, 'is': 2, 'rose': 3}
 
-#     It's fine to consider punctuation part of a word (e.g., a comma
-#     at the end of a word can be counted as part of that word) and
-#     to consider differently-capitalized words as different::
+    It's fine to consider punctuation part of a word (e.g., a comma
+    at the end of a word can be counted as part of that word) and
+    to consider differently-capitalized words as different::
 
-#         >>> print_dict(count_words("Porcupine see, porcupine do."))
-#         {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
-#     """
-#     new_phrase = phrase.split(" ")
-#     dictionary = {}
-#     for word in new_phrase:
-#         if word in dictionary:
-#             dictionary[word] += 1
-#         else:
-#             dictionary[word] = 1
-#     return dictionary
+        >>> print_dict(count_words("Porcupine see, porcupine do."))
+        {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
+    """
+    new_phrase = phrase.split(" ")
+    dictionary = {}
+    for word in new_phrase:
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+    return dictionary
 
-# print(count_words("Porcupine see, porcupine do."))
+print(count_words("Porcupine see, porcupine do."))
 
 
-# def print_melon_at_price(price):
-#     """Given a price, print all melons available at that price, in alphabetical order.
+def print_melon_at_price(price):
+    """Given a price, print all melons available at that price, in alphabetical order.
 
-#     Here are a list of melon names and prices:
+    Here are a list of melon names and prices:
 
-#     Honeydew 2.50
-#     Cantaloupe 2.50
-#     Watermelon 2.95
-#     Musk 3.25
-#     Crenshaw 3.25
-#     Christmas 14.25
-#     (it was a bad year for Christmas melons -- supply is low!)
+    Honeydew 2.50
+    Cantaloupe 2.50
+    Watermelon 2.95
+    Musk 3.25
+    Crenshaw 3.25
+    Christmas 14.25
+    (it was a bad year for Christmas melons -- supply is low!)
 
-#     If there are no melons at that price print "None found"
+    If there are no melons at that price print "None found"
 
-#         >>> print_melon_at_price(2.50)
-#         Cantaloupe
-#         Honeydew
+        >>> print_melon_at_price(2.50)
+        Cantaloupe
+        Honeydew
 
-#         >>> print_melon_at_price(2.95)
-#         Watermelon
+        >>> print_melon_at_price(2.95)
+        Watermelon
 
-#         >>> print_melon_at_price(5.50)
-#         # None found
-# #     """
-#     melon={
-#         '2.50': ("Honeydew","Cantaloupe"),
-#        '2.95': ("Watermelon"),
-#        '3.25': ("Musk","Crewshaw"),
-#         '14.25': "Christmas"
+        >>> print_melon_at_price(5.50)
+        # None found
+    """
+    melon={
+        '2.50': ("Honeydew","Cantaloupe"),
+       '2.95': ("Watermelon"),
+       '3.25': ("Musk","Crewshaw"),
+        '14.25': "Christmas"
 
-#     }
+    }
 
-#     if melon.get(price):
-#         melons = melon[price]
-#         for melon in melons:
-#             print(melon)
-#     else:
-#         return "None found"
+    if melon.get(price):
+        melons = melon[price]
+        for melon in melons:
+            print(melon)
+    else:
+        return "None found"
 
-# print_melon_at_price('2.50')
+print_melon_at_price('2.50')
 
 
 
 
 def translate_to_pirate_talk(phrase):
-#     """Translate phrase to pirate talk.
+    """Translate phrase to pirate talk.
 
-#     Given a phrase, translate each word to the Pirate-speak
-#     equivalent. Words that cannot be translated into Pirate-speak
-#     should pass through unchanged. Return the resulting sentence.
+    Given a phrase, translate each word to the Pirate-speak
+    equivalent. Words that cannot be translated into Pirate-speak
+    should pass through unchanged. Return the resulting sentence.
 
-#     Here's a table of English to Pirate translations:
+    Here's a table of English to Pirate translations:
 
-#     ----------  ----------------
-#     English     Pirate
-#     ----------  ----------------
-#     sir         matey
-#     hotel       fleabag inn
-#     student     swabbie
-#     man         matey
-#     professor   foul blaggart
-#     restaurant  galley
-#     your        yer
-#     excuse      arr
-#     students    swabbies
-#     are         be
-#     restroom    head
-#     my          me
-#     is          be
-#     ----------  ----------------
+    ----------  ----------------
+    English     Pirate
+    ----------  ----------------
+    sir         matey
+    hotel       fleabag inn
+    student     swabbie
+    man         matey
+    professor   foul blaggart
+    restaurant  galley
+    your        yer
+    excuse      arr
+    students    swabbies
+    are         be
+    restroom    head
+    my          me
+    is          be
+    ----------  ----------------
    
-#     For example::
+    For example::
 
-#         >>> translate_to_pirate_talk("my student is not a man")
-#         'me swabbie be not a matey'
+        >>> translate_to_pirate_talk("my student is not a man")
+        'me swabbie be not a matey'
 
-#     You should treat words with punctuation as if they were different
-#     words::
+    You should treat words with punctuation as if they were different
+    words::
 
-#         >>> translate_to_pirate_talk("my student is not a man!")
-#         'me swabbie be not a man!'
-#     """
+        >>> translate_to_pirate_talk("my student is not a man!")
+        'me swabbie be not a man!'
+    """
 
 
         dictionary = {
